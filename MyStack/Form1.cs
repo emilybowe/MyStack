7e1f0ded21da6c3b1b60ae3c3251f972e64c535a
10389679 <10389679@mydbs.ie>
2019-04-09 19:04:18 +0100

    first edit

--- MyStack/Form1.cs
+++ MyStack/Form1.cs
@@ -27,5 +27,10 @@ namespace MyStack
         {
             textBox1.Text = "" + s.pop();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
