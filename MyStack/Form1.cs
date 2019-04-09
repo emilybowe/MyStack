b256c2d5e07605fd769f1f6626c6ec579ee31d4f
Emily Bowe <10389679@mydbs.ie>
2019-04-09 18:53:15 +0100

    Add project files.

---
+++ MyStack/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyStack
+{
+    public partial class Form1 : Form
+    {
+        StackOfInt s = new StackOfInt();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s.push(int.Parse(textBox1.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + s.pop();
+        }
+    }
+}
---
+++ MyStack/StackOfInt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+    class StackOfInt
+    {
+        int[] data= new int[10];
+        int index = 0;
+        public void push(int v)
+        {
+            data[index++] = v;
+        }
+        public int pop()
+        {
+            return data[--index];
+        }
+    }
+}
